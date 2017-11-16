010493d6fc541d9a46019d94b9ef7fae37f67358
Stefan Thorpe <stefan.thorpe@gmail.com>
2017-11-16 10:51:05 +0700

    Added basic view

---
+++ storage/framework/views/b18d705f49d4cb87cb2bde363b3ca5bd2bbb5823.php
@@ -0,0 +1,9 @@
+<html class="gr__localhost">
+  <head><style></style></head>
+  <body style="background: #1CABE3;" data-gr-c-s-loaded="true" cz-shortcut-listen="true">
+    <img src="http://caylent.com/wp-content/themes/website/img/caylent-logo--white.svg" style="width: 120px; margin: 15px">
+    <p style="padding-top: 100px;width: 100%;color: white;font-size: 80px;font-family: varela round, sans-serif;text-align: center;"><?php echo env('DISPLAY', 'Hello World'); ?></p>
+    <p style="text-align: center;color: white;font-size: 30px;position: absolute;bottom: 40px;width: 30%;left: 35%;font-family: varela round, sans-serif;">#DevOpsIsNow</p>
+    <p style="text-align: center;color: white;font-size: 30px;position: absolute;bottom: 10px;width: 30%;left: 35%;font-family: varela round, sans-serif;">@caylentinc</p>
+  </body>
+</html>
